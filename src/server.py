4c0cc2ef5656b59c3455386deb5146044d099e99
Calvin Laurenson <89622328+calvin-laurenson@users.noreply.github.com>
2022-03-12 08:38:55 -0800

    logger and basic auth

--- src/server.py
+++ src/server.py
@@ -1,10 +1,17 @@
 import json
-from fastapi import FastAPI
+import time
+from fastapi import FastAPI, HTTPException, Request, Security
 from fastapi.responses import HTMLResponse
+from fastapi.security import APIKeyHeader
+from fastapi import status
 import pymongo
+from pymongo.database import Database
+from loguru import logger
 import os
 
 from util import all_files_in_dir, serialize_documents, strip_extension
+
+api_key_header_auth = APIKeyHeader(name="Authorization", auto_error=True)
 
 MONGO_URI = os.environ.get("MONGO_URI")
 
@@ -34,9 +41,35 @@ team_lists = list(map(strip_extension, all_files_in_dir("../team-lists")))
 match_schedules = list(map(strip_extension, all_files_in_dir("../match-schedules")))
 
 client = pymongo.MongoClient(MONGO_URI)
-db = client[DB_NAME]
+# db = client[DB_NAME]
+api_db = client["api"]
+creds_collection = api_db["credentials"]
+
+creds = serialize_documents(creds_collection.find())
 
 app = FastAPI()
+
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    process_time = time.time() - start_time
+    response.headers["X-Process-Time"] = str(process_time)
+    return response
+
+async def get_api_key(api_key_header: str = Security(api_key_header_auth)):
+    logger.debug(f"api_key_header: {api_key_header}")
+    logger.debug(f"cred length: {len(creds)}")
+    for cred in creds:
+        logger.debug(f"cred: {cred}")
+        if cred["api_key"] == api_key_header:
+            logger.debug("passed api key auth")
+            return cred
+    logger.debug("failed api key auth")
+    raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid API Key",
+        )
 
 
 @app.get("/", response_class=HTMLResponse)
@@ -44,8 +77,9 @@ def read_root():
     return """<h1>Welcome to Grosbeak</h1>"""
 
 
-@app.get("/collection/{collection_name}")
-def read_collection(collection_name: str):
+@app.get("/collection/{collection_name}", dependencies=[Security(get_api_key)])
+def read_collection(collection_name: str, event_key: str = DB_NAME):
+    db = client[event_key]
     if (
         collection_name in allowed_collections
         and collection_name in db.list_collection_names()

1a93ab66e5d66f412797741d667cd5ab4a1e4cac
Calvin Laurenson <89622328+calvin-laurenson@users.noreply.github.com>
2022-04-08 20:43:04 -0700

    actually save new credentials :|

--- src/routers/admin.py
+++ src/routers/admin.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter, Depends
 from pydantic import BaseModel
 
 from ..auth import get_api_key
-
+from ..db import api_db
 
 router = APIRouter(prefix="/admin")
 
@@ -21,8 +21,10 @@ class NewCredentialResponse(BaseModel):
 @router.post("/new-credential", response_model=NewCredentialResponse)
 async def create_credential(*, data: NewCredentialRequest, user_level: dict = Depends(get_api_key)):
     if user_level["level"] >= 2:
-        return NewCredentialResponse(
+        creds = NewCredentialResponse(
             description=data.description,
             level=data.level,
             api_key=str(uuid.uuid4()).replace("-", ""),
         )
+        api_db["credentials"].insert_one(creds.dict())
+        return creds

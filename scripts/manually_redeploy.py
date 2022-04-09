5f431a064d358d03c108ddb6384dd3fcab2b6734
Calvin Laurenson <89622328+calvin-laurenson@users.noreply.github.com>
2022-04-08 18:32:06 -0700

    fix redeploy script

--- scripts/manually_redeploy.py
+++ scripts/manually_redeploy.py
@@ -11,7 +11,7 @@ def make_env_args(env: Env):
     config = env.dict()
     env_list = []
     for key, value in config.items():
-        env_list.append(f"--env {key}={value}")
+        env_list.append(f"--env \"{key}={value}\"")
     return " ".join(env_list)
 
 def deploy(config: Env):
@@ -22,11 +22,24 @@ def deploy(config: Env):
     REMOVE_IMAGE = f"docker rmi {FULL_IMAGE_NAME}"
     PULL_IMAGE = f"docker pull {FULL_IMAGE_NAME}"
     START_CONTAINER = f"docker run -d --name {CONTAINER_NAME} -p 8002:80 {make_env_args(config)} {FULL_IMAGE_NAME}"
-    final_command = "; ".join([STOP_CONTAINER, REMOVE_IMAGE, PULL_IMAGE, START_CONTAINER])
+    final_command = " ; ".join([STOP_CONTAINER, REMOVE_IMAGE, PULL_IMAGE, START_CONTAINER])
     print(final_command)
     if input("Does this command look good? (y/N): ").lower() == "y":
-        print(subprocess.check_output(["ssh", "picklist@citrus.ant.isi.edu", final_command]))
-
+        ssh_process = subprocess.Popen(['ssh', 
+                               "picklist@citrus.ant.isi.edu"],
+                               stdin=subprocess.PIPE, 
+                               stdout = subprocess.PIPE,
+                               universal_newlines=True,
+                               bufsize=0)
+        ssh_process.stdin.write(STOP_CONTAINER + "\n")
+        ssh_process.stdin.write(REMOVE_IMAGE + "\n")
+        ssh_process.stdin.write(PULL_IMAGE + "\n")
+        ssh_process.stdin.write(START_CONTAINER + "\n")
+        ssh_process.stdin.close()
+        for line in ssh_process.stdout:
+            if line == "END\n":
+                break
+            print(line,end="")
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Manually redeploy the grosbeak container on prod")
     parser.add_argument("env_file", help="The .env file to use")

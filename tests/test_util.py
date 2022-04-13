b16b1b72aad9dc03b05d877a9c2aff0e4e19adec
Calvin Laurenson <89622328+calvin-laurenson@users.noreply.github.com>
2022-04-13 13:07:28 -0700

    setup pytest and rename code.yml

---
+++ tests/test_util.py
@@ -0,0 +1,5 @@
+import grosbeak.util as util
+
+def test_strip_extension():
+    assert util.strip_extension("test_file.txt") == "test_file"
+    assert util.strip_extension("test_file.test.json") == "test_file"

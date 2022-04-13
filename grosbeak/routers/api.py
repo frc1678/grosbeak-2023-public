4328f8117094651d327a9c37a1c2a4e3515e809f
Calvin Laurenson <89622328+calvin-laurenson@users.noreply.github.com>
2022-04-13 16:42:03 -0700

    format

--- grosbeak/routers/api.py
+++ grosbeak/routers/api.py
@@ -9,10 +9,13 @@ from ..db import client, allowed_collections
 from ..env import env
 from ..util import all_files_in_dir, serialize_documents, strip_extension
 import json
+
 router = APIRouter(prefix="/api", dependencies=[Security(get_api_key)])
+
 
 class ErrorMessage(BaseModel):
     error: str
+
 
 @router.get("/collection/{collection_name}", responses={404: {"model": ErrorMessage}})
 def read_collection(collection_name: str, event_key: str = env.DB_NAME):
@@ -24,7 +27,9 @@ def read_collection(collection_name: str, event_key: str = env.DB_NAME):
         collection = db[collection_name]
         return serialize_documents(list(collection.find()))
     else:
-        return JSONResponse(content={"error": "Collection not found/allowed"}, status_code=404)
+        return JSONResponse(
+            content={"error": "Collection not found/allowed"}, status_code=404
+        )
 
 
 class ColorEnum(str, Enum):
@@ -41,14 +46,23 @@ class MatchScheduleMatch(BaseModel):
     teams: List[MatchScheduleTeam]
 
 
-
-@router.get("/match-schedule/{event_key}", response_model=Dict[str, MatchScheduleMatch], responses={404: {"model": ErrorMessage}})
+@router.get(
+    "/match-schedule/{event_key}",
+    response_model=Dict[str, MatchScheduleMatch],
+    responses={404: {"model": ErrorMessage}},
+)
 def read_match_schedule(event_key: str):
     return read_static_json("match-schedules", event_key)
 
-@router.get("/team-list/{event_key}", response_model=List[str], responses={404: {"model": ErrorMessage}})
+
+@router.get(
+    "/team-list/{event_key}",
+    response_model=List[str],
+    responses={404: {"model": ErrorMessage}},
+)
 def read_team_list(event_key: str):
     return read_static_json("team-lists", event_key)
+
 
 def read_static_json(folder: str, key: str):
     path = os.path.realpath(f"./static/{folder}/{key}.json")
